3bc1bfee6f5cba2d456b04c035501fcc318eebeb
Kr1tX12 <nion19255@gmail.com>
2025-07-18 10:43:33 +0300

    фикс ошибки init not found

--- index.ts
+++ index.ts
@@ -21,40 +21,42 @@ interface ProviderOptions {
   apiKey: string;
 }
 
-export default ({ config }: { config: ProviderOptions & Settings }) => {
-  const resend = new Resend(config.apiKey);
+export default {
+  init(providerOptions: ProviderOptions, settings: Settings) {
+    const resend = new Resend(providerOptions.apiKey);
 
-  return {
-    send: async (options: SendOptions): Promise<void> => {
-      const {
-        from = config.defaultFrom,
-        replyTo = config.defaultReplyTo,
-        to,
-        cc,
-        bcc,
-        subject,
-        text,
-        html,
-        ...rest
-      } = options;
-
-      try {
-        await resend.emails.send({
-          from,
+    return {
+      send: async (options: SendOptions): Promise<void> => {
+        const {
+          from = settings.defaultFrom,
+          replyTo = settings.defaultReplyTo,
           to,
           cc,
           bcc,
-          replyTo,
           subject,
           text,
           html,
-          react: null,
-          ...rest,
-        });
-      } catch (error) {
-        console.error("Resend email error:", error);
-        throw error;
-      }
-    },
-  };
+          ...rest
+        } = options;
+
+        try {
+          await resend.emails.send({
+            from,
+            to,
+            cc,
+            bcc,
+            replyTo,
+            subject,
+            text,
+            html,
+            react: undefined,
+            ...rest,
+          });
+        } catch (error) {
+          console.error("Resend email error:", error);
+          throw error;
+        }
+      },
+    };
+  },
 };
